0d7e4116b247210c7156e12856769b21b42d6bbf
alexmruddock <alex.ruddock@afinea.com>
2023-11-12 19:57:50 +0000

    it can generate surveys!

--- src/App.js
+++ src/App.js
@@ -6,16 +6,44 @@ function App() {
   const [survey, setSurvey] = useState(null);
 
   const handleSubmit = async (description) => {
+    console.log("handleSubmit called with:", description);
+    
     // Call the API to create the survey
-    const response = await fetch('https://bookish-pancake-q7w7vvr66ggfxp5j-3000.app.github.dev/api/generate-survey', {
+    const response = await fetch('https://bookish-pancake-q7w7vvr66ggfxp5j-3000.app.github.dev/generate-survey', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ description }),
+    }).catch(error => {
+      console.error("Fetch error:", error);
+      return;
     });
-    const data = await response.json();
-    setSurvey(data.survey);
+    
+    if (!response) {
+      console.error("No response from fetch");
+      return;
+    }
+    
+    console.log(response);
+    
+    if (response && response.ok) {
+      try {
+        const data = await response.json();
+        console.log("Parsed data:", data);
+
+        // Parse the 'survey' field from string to JSON object
+        const surveyObject = JSON.parse(data.survey);
+
+        // Update the state with the parsed object
+        setSurvey(surveyObject);
+
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+      }
+    } else {
+      console.error("Response error:", response.status);
+    }
   };
 
   return (
--- src/SurveyDisplay.js
+++ src/SurveyDisplay.js
@@ -1,20 +1,53 @@
 import React from 'react';
 
 function SurveyDisplay({ survey }) {
-  if (!survey || survey.questions.length === 0) {
+  if (!survey || !survey.questions || survey.questions.length === 0) {
     return <p>No survey questions to display.</p>;
   }
 
   return (
     <div>
       <h2>{survey.title}</h2>
-      <ul>
-        {survey.questions.map((question, index) => (
-          <li key={index}>{question}</li>
-        ))}
-      </ul>
+      <p>{survey.description}</p>
+      {survey.questions.map((question, index) => renderQuestion(question, index))}
     </div>
   );
+}
+
+function renderQuestion(question, index) {
+  switch (question.question_type) {
+    case 'multiple_choice':
+      return (
+        <div key={index}>
+          <p>{question.question}</p>
+          {question.options.map(option => (
+            <label key={option}>
+              <input type="radio" name={`question-${index}`} value={option} />
+              {option}
+            </label>
+          ))}
+        </div>
+      );
+
+    case 'rating':
+      return (
+        <div key={index}>
+          <p>{question.question}</p>
+          <input type="range" min="1" max={question.scale} />
+        </div>
+      );
+
+    case 'open_ended':
+      return (
+        <div key={index}>
+          <p>{question.question}</p>
+          <textarea name={`question-${index}`} />
+        </div>
+      );
+
+    default:
+      return <p key={index}>Unsupported question type: {question.type}</p>;
+  }
 }
 
 export default SurveyDisplay;
